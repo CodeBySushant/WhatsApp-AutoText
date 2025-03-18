ad4954c90aa130cb595511f00003c1903f88be5e
Sushant Sharma <itsofficialsushant@gmail.com>
2025-03-18 20:58:18 +0530

    Added Counter to send n number of texts

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pywhatkit as pwk
 import pyautogui
+import keyboard
 import time
 
-phone_number = "+916204653603"
-message = "Hello World!"
+phone_number = "+9779860731062"
+message = "Hii bsdk!"
 
-# Send message instantly
+# Open chat and send the first message to activate the window
 pwk.sendwhatmsg_instantly(phone_number, message, wait_time=10, tab_close=False)
 
-# Short wait: Give the browser time to type the message (experiment with this)
-time.sleep(3)  # You can try 2 if it's fast enough
-
-# Instantly press Enter
+time.sleep(3)
+# Press enter to send the message
 pyautogui.press("enter")
+time.sleep(2)
 
-print("Done! Message sent instantly.")
+for i in range(999):
+    if keyboard.is_pressed('esc'):  # Press 'esc' to stop
+        print("Loop stopped by user!")
+        break
+
+    pyautogui.typewrite(f"{message} {i + 2}")
+    pyautogui.press("enter")
+    time.sleep(0.2)
+
+print("Done or stopped!")
